e1fd57cabd112996eb1d2e3ab8fb43dd4a0176da
DeF <DeF-2008@yandex.ru>
2024-07-21 01:01:48 +0300

    hw

---
+++ tests/LoginNetology.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@playwright/test");
+const {email, password, incorrectEmail, incorrectPassword} = require("../user.js");
+
+
+test("Failed authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', incorrectEmail);
+  await page.fill('[placeholder="Пароль"]', incorrectPassword);
+  await page.click('[data-testid="login-submit-btn"]');
+  const error = await page.locator('[data-testid="login-error-hint"]');
+  await expect(error).toHaveText("Вы ввели неправильно логин или пароль.");
+  await page.screenshot({ path: "screenshotFailed.png", fullPage: true });
+}, 5000);
+
+test("Successful authorization", async ({ page }) => {
+   await page.goto("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', email);
+  await page.fill('[placeholder="Пароль"]', password);
+  await page.click('[data-testid="login-submit-btn"]');
+  await expect(page.locator("h2")).toContainText(["Моё обучение"]);
+  await page.screenshot({ path: "screenshotSuccessful.png", fullPage: true });
+}, 5000);
